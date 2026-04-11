d198abd3f0fbcd4d7dcc8223ee8f320407927adb
hato810424 <74600008+hato810424@users.noreply.github.com>
2026-04-11 23:59:03 +0900

    init

---
+++ apps/auth/src/index.ts
@@ -0,0 +1,34 @@
+import { readFile } from 'node:fs/promises'
+import { dirname, join, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import { serve } from '@hono/node-server'
+import { serveStatic } from '@hono/node-server/serve-static'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const webDistRoot = resolve(__dirname, '../../web/dist')
+const isProd = process.env.NODE_ENV === 'production'
+
+if (isProd) {
+  app.use('/*', serveStatic({ root: webDistRoot }))
+  app.get('/*', async (c) => {
+    return c.html(await readFile(join(webDistRoot, 'index.html'), 'utf-8'))
+  })
+} else {
+  app.get('/', (c) => {
+    return c.text('Hello Hono!')
+  })
+}
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3001,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`)
+  },
+)
